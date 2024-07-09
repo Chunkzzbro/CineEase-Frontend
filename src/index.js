4b01a70b0842a935cecabb8af1de53bbc42d34fe
Chunkzzbro <renjithaniltvm@gmail.com>
2024-07-09 16:07:09 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import axios from "axios";
 import { Provider } from "react-redux";
 import { store } from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://cineease-backend.onrender.com";
 root.render(
   <React.StrictMode>
     <BrowserRouter>
